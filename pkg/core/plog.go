50713c47ae87ad2c1e221cf1df76dab0d0fef792
Aditya Kumar Varanasi <aditya@macbook-pro.myfiosgateway.com>
2022-08-29 07:53:43 -0400

    update

--- pkg/core/plog.go
+++ pkg/core/plog.go
@@ -9,20 +9,21 @@ func Init() {
 		DisableColors: true,
 		FullTimestamp: true,
 	})
+	//log.SetLevel(log.DebugLevel) //Note this must be enabled from the application
 }
 
-func Info(args interface{}) {
+func Info(args ...interface{}) {
 	log.Info(args)
 }
 
-func Debug(args interface{}) {
+func Debug(args ...interface{}) {
 	log.Debug(args)
 }
 
-func Warn(args interface{}) {
+func Warn(args ...interface{}) {
 	log.Warn(args)
 }
 
-func Error(args interface{}) {
+func Error(args ...interface{}) {
 	log.Error(args)
 }
