4137bd3491e485241d7a41ee3089d3ca2a67105b
Swechhya <swechhyabista@gmail.com>
2022-09-01 21:07:21 +0545

    remove sprintf method

--- test/plog_test.go
+++ test/plog_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	logger "github.com/phil-inc/plog-ng/pkg/core"
@@ -9,8 +8,8 @@ import (
 
 func TestConnectivity(t *testing.T) {
 	logger.Init()
-	logger.Info(fmt.Sprintf("test message %s", "special value"))
-	logger.Debug(fmt.Sprintf("test message %s", "special value"))
-	logger.Error(fmt.Sprintf("test message %s", "special value"))
-	logger.Warn(fmt.Sprintf("test message %s", "special value"))
+	logger.Info("test message")
+	logger.Debug("test message")
+	logger.Error("test message")
+	logger.Warn("test message")
 }
