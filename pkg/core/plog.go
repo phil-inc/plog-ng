ae133e592a36e2b34c36468b45c94fe52908b316
Swechhya <swechhyabista@gmail.com>
2022-09-01 13:41:11 +0545

    Add package, file and function info

--- pkg/core/plog.go
+++ pkg/core/plog.go
@@ -1,29 +1,64 @@
 package core
 
 import (
+	"runtime"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func Init() {
-	log.SetFormatter(&log.TextFormatter{
-		DisableColors: true,
-		FullTimestamp: true,
-	})
-	//log.SetLevel(log.DebugLevel) //Note this must be enabled from the application
+	log.SetFormatter(&log.JSONFormatter{})
+	// log.SetLevel(log.DebugLevel) //Note this must be enabled from the application
 }
 
-func Info(args ...interface{}) {
-	log.Info(args)
+func Info(msg string) {
+	pc, file, _, ok := runtime.Caller(1)
+	if ok {
+		log.WithFields(getAdditionalFields(pc, file)).Info(msg)
+		return
+	}
+
+	log.Info(msg)
 }
 
-func Debug(args ...interface{}) {
-	log.Debug(args)
+func Debug(msg string) {
+	pc, file, _, ok := runtime.Caller(1)
+	if ok {
+		log.WithFields(getAdditionalFields(pc, file)).Debug(msg)
+		return
+	}
+
+	log.Debug(msg)
 }
 
-func Warn(args ...interface{}) {
-	log.Warn(args)
+func Warn(msg string) {
+	pc, file, _, ok := runtime.Caller(1)
+	if ok {
+		log.WithFields(getAdditionalFields(pc, file)).Warn(msg)
+		return
+	}
 }
 
-func Error(args ...interface{}) {
-	log.Error(args)
+func Error(msg string) {
+	pc, file, _, ok := runtime.Caller(1)
+	if ok {
+		log.WithFields(getAdditionalFields(pc, file)).Error(msg)
+		return
+	}
+}
+
+func getAdditionalFields(pc uintptr, file string) log.Fields {
+	details := runtime.FuncForPC(pc)
+	if details != nil {
+		fn := strings.Split(details.Name(), ".")
+		fields := log.Fields{
+			"package":  fn[0],
+			"function": fn[1],
+			"file":     file,
+		}
+		return fields
+	}
+
+	return log.Fields{}
 }
--- test/plog_test.go
+++ test/plog_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	logger "github.com/phil-inc/plog-ng/pkg/core"
@@ -8,8 +9,8 @@ import (
 
 func TestConnectivity(t *testing.T) {
 	logger.Init()
-	logger.Info("test message %s", "special value")
-	logger.Debug("test message %s", "special value")
-	logger.Error("test message %s", "special value")
-	logger.Warn("test message %s", "special value")
+	logger.Info(fmt.Sprintf("test message %s", "special value"))
+	logger.Debug(fmt.Sprintf("test message %s", "special value"))
+	logger.Error(fmt.Sprintf("test message %s", "special value"))
+	logger.Warn(fmt.Sprintf("test message %s", "special value"))
 }
