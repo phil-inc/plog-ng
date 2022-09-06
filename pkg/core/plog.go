07204045439db64399f21b26528d0d4298cfb078
Swechhya <swechhyabista@gmail.com>
2022-09-06 13:06:09 -0700

    add logger with additional fields

--- pkg/core/plog.go
+++ pkg/core/plog.go
@@ -8,6 +8,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type Fields map[string]interface{}
+
 func Init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	// log.SetLevel(log.DebugLevel) //Note this must be enabled from the application
@@ -33,6 +35,25 @@ func Infof(s string, args ...interface{}) {
 	log.Infof(s, args...)
 }
 
+func InfoWithAdditionalFields(s string, args interface{}) {
+	pc, file, line, ok := runtime.Caller(1)
+	if ok {
+		l := log.WithFields(getAdditionalFields(pc, file, line))
+		l = mergeAddionalFields(l, args)
+		l.Info(s)
+		return
+	}
+
+	af, ok := args.(map[string]interface{})
+	if ok {
+		l := log.WithFields(af)
+		l.Info(s)
+		return
+	}
+
+	log.Info(s)
+}
+
 func Debug(msg string) {
 	pc, file, line, ok := runtime.Caller(1)
 	if ok {
@@ -51,6 +72,25 @@ func Debugf(s string, args ...interface{}) {
 	}
 
 	log.Debugf(s, args...)
+}
+
+func DebugWithAdditionalFields(s string, args interface{}) {
+	pc, file, line, ok := runtime.Caller(1)
+	if ok {
+		l := log.WithFields(getAdditionalFields(pc, file, line))
+		l = mergeAddionalFields(l, args)
+		l.Debug(s)
+		return
+	}
+
+	af, ok := args.(map[string]interface{})
+	if ok {
+		l := log.WithFields(af)
+		l.Debug(s)
+		return
+	}
+
+	log.Debug(s)
 }
 
 func Warn(msg string) {
@@ -73,6 +113,25 @@ func Warnf(s string, args ...interface{}) {
 	log.Warnf(s, args...)
 }
 
+func WarnWithAdditionalFields(s string, args interface{}) {
+	pc, file, line, ok := runtime.Caller(1)
+	if ok {
+		l := log.WithFields(getAdditionalFields(pc, file, line))
+		l = mergeAddionalFields(l, args)
+		l.Warn(s)
+		return
+	}
+
+	af, ok := args.(map[string]interface{})
+	if ok {
+		l := log.WithFields(af)
+		l.Warn(s)
+		return
+	}
+
+	log.Warn(s)
+}
+
 func Error(msg string) {
 	pc, file, line, ok := runtime.Caller(1)
 	if ok {
@@ -93,6 +152,25 @@ func Errorf(s string, args ...interface{}) {
 	log.Errorf(s, args...)
 }
 
+func ErrorWithAdditionalFields(s string, args interface{}) {
+	pc, file, line, ok := runtime.Caller(1)
+	if ok {
+		l := log.WithFields(getAdditionalFields(pc, file, line))
+		l = mergeAddionalFields(l, args)
+		l.Error(s)
+		return
+	}
+
+	af, ok := args.(map[string]interface{})
+	if ok {
+		l := log.WithFields(af)
+		l.Error(s)
+		return
+	}
+
+	log.Error(s)
+}
+
 func getAdditionalFields(pc uintptr, file string, line int) log.Fields {
 	details := runtime.FuncForPC(pc)
 	if details != nil {
@@ -106,4 +184,14 @@ func getAdditionalFields(pc uintptr, file string, line int) log.Fields {
 	}
 
 	return log.Fields{}
+}
+
+func mergeAddionalFields(l *log.Entry, args interface{}) *log.Entry {
+	af, ok := args.(map[string]interface{})
+	if ok {
+		l = l.WithFields(af)
+		return l
+	}
+
+	return l
 }
